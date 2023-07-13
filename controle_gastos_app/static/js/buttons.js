0a012fa86b50bf12de560ed784bbf7628b63e642
Vinicius Bender <viniciusbender01@outlook.com>
2023-07-13 02:53:55 -0300

    50%

--- controle_gastos_app/static/js/buttons.js
+++ controle_gastos_app/static/js/buttons.js
@@ -1,14 +1,10 @@
 let btnEntrada = document.getElementById("btn-entrada");
 let btnSaida = document.getElementById("btn-saida");
-let entradaSelected = false;
-let saidaSelected = false;
 
 btnEntrada.addEventListener("click", () => {
 
     btnEntrada.style.backgroundColor = "black";
     btnSaida.style.backgroundColor = "#363636";
-    entradaSelected = true;
-    saidaSelected = false;
 
 });
 
@@ -16,6 +12,4 @@ btnSaida.addEventListener("click", () => {
 
     btnSaida.style.backgroundColor = "black";
     btnEntrada.style.backgroundColor = "#363636";
-    entradaSelected = false;
-    saidaSelected = true;
 });
