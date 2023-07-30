f349e22a04d1f5be2114bc1e7404ccc3733246b0
Vinicius Bender <viniciusbender01@outlook.com>
2023-07-30 03:00:21 -0300

    100%

--- controle_gastos_projeto/settings.py
+++ controle_gastos_projeto/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,17 +78,18 @@ WSGI_APPLICATION = 'controle_gastos_projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'controle_gastosdb',
-       'USER': 'postgres',
-       'PASSWORD': 'admin',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
-
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'controle_gastosdb',
+#        'USER': 'postgres',
+#        'PASSWORD': 'admin',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+# }
+DATABASE_URL = "postgres://controle_gastosdb_user:pnHNok4BSgp2F9Y0oqMH8AVtl3RMW2Ds@dpg-cj2vc4h8g3n1jkh59lkg-a.oregon-postgres.render.com/controle_gastosdb"
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -124,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 LOGIN_REDIRECT_URL = '/'
 
--- controle_gastos_projeto/urls.py
+++ controle_gastos_projeto/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from controle_gastos_app  import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -12,3 +13,5 @@ urlpatterns = [
     path('detalhes_transacao/<int:id>/', views.detalhes_transacao, name='detalhes_transacao'),
     path('listagem/', views.listagem, name='listagem'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
